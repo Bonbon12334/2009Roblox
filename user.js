6be80121fd41e1b468a3d97993904e4e62698f0b
Bonbon12334 <83611734+Bonbon12334@users.noreply.github.com>
2025-02-08 18:06:30 -0500

    user.js

---
+++ user.js
@@ -0,0 +1,3 @@
+// user.js: Dynamically sets the username (this can be changed later to fetch real user data)
+const username = "RandomUser"; // This is static for now
+document.getElementById('username').textContent = username;
